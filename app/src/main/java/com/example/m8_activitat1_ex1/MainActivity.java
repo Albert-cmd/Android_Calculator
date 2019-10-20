4670296c530a034a211380e9e2c985dab6bd8338
Albert Gonzalez <albert1999dual@gmail.com>
2019-10-20 16:29:26 +0200

    Variables de numeros inicializadas y botones definidos.

--- app/src/main/java/com/example/m8_activitat1_ex1/MainActivity.java
+++ app/src/main/java/com/example/m8_activitat1_ex1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.m8_activitat1_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        EditText EditText;
+
+
+        Button b0, b1, b2, b3, b4, b5, b6,
+                b7, b8, b9, bsuma, bmultiplica,
+                bresta, besborra, bigual,bcoma;
+
+        float valor1, valor2;
+
+        boolean suma, resta, multiplica;
 
 
 
