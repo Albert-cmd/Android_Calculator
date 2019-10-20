206659b4a65a259413bba573311898252fce828e
Albert Gonzalez <albert1999dual@gmail.com>
2019-10-20 16:50:07 +0200

    Variables de numeros inicializadas y botones definidos.

--- app/src/main/java/com/example/m8_activitat1_ex1/MainActivity.java
+++ app/src/main/java/com/example/m8_activitat1_ex1/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.m8_activitat1_ex1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -13,7 +14,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EditText EditText;
+        final EditText EditText;
 
 
         Button b0, b1, b2, b3, b4, b5, b6,
@@ -25,6 +26,104 @@ public class MainActivity extends AppCompatActivity {
         boolean suma, resta, multiplica;
 
 
+        b0 = (Button) findViewById(R.id.b0);
+        b1 = (Button) findViewById(R.id.b1);
+        b2 = (Button) findViewById(R.id.b2);
+        b3 = (Button) findViewById(R.id.b3);
+        b4 = (Button) findViewById(R.id.b4);
+        b5 = (Button) findViewById(R.id.b5);
+        b6 = (Button) findViewById(R.id.b6);
+        b7 = (Button) findViewById(R.id.b7);
+        b8 = (Button) findViewById(R.id.b8);
+        b9 = (Button) findViewById(R.id.b9);
+
+        bsuma = (Button) findViewById(R.id.suma);
+        bmultiplica = (Button) findViewById(R.id.multiplica);
+        bresta = (Button) findViewById(R.id.resta);
+        besborra = (Button) findViewById(R.id.esborra);
+        bigual = (Button) findViewById(R.id.igual);
+        bcoma = (Button) findViewById(R.id.coma);
+
+
+        EditText = (EditText) findViewById(R.id.EditText);
+
+
+
+
+
+
+             // METODES PROPIS PER CALCULAR
+
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "1");
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "2");
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "3");
+            }
+        });
+
+        b4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "4");
+            }
+        });
+
+        b5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "5");
+            }
+        });
+
+        b6.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "6");
+            }
+        });
+
+        b7.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "7");
+            }
+        });
+
+        b8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "8");
+            }
+        });
+
+        b9.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "9");
+            }
+        });
+
+        b0.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                EditText.setText(EditText.getText() + "0");
+            }
+        });
 
 
 
